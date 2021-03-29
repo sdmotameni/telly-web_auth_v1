5816ae9e40c693fdd6f9e8775dae73ac56785ea7
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-29 19:45:34 -0400

    Added header

--- pages/profile.js
+++ pages/profile.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Head from "next/head";
 import ProfileHeader from "../components/profileHeader";
 import LinkButton from "../components/linkButton";
 import Navbar from "../components/navbar";
@@ -11,28 +12,34 @@ export default class Profile extends Component {
   render() {
     const { isAdmin } = this.state;
     return (
-      <main className="w-full h-screen bg-gray-100">
-        {isAdmin && <Navbar name="Adam" />}
-        <div className="p-4 mx-auto">
-          <ProfileHeader name="Sep Motameni" bio="High Point University" />
-          {isAdmin && (
-            <div className="pt-2">
-              <EditButtons />
+      <div>
+        <Head>
+          <title>Sep Motameni - Telly</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="w-full h-screen bg-gray-100">
+          {isAdmin && <Navbar name="Adam" />}
+          <div className="p-4 mx-auto">
+            <ProfileHeader name="Sep Motameni" bio="High Point University" />
+            {isAdmin && (
+              <div className="pt-2">
+                <EditButtons />
+              </div>
+            )}
+            <div className="mt-4">
+              {LinkButton("instagram", "sepmotameni")}
+              {LinkButton("snapchat", "sepmotameni")}
+              {LinkButton("tiktok", "sepmotameni")}
+              {LinkButton("cashapp", "sdmotameni")}
+              {LinkButton("linkedin", "sepmotameni")}
+              {LinkButton("venmo", "sdmotameni")}
+              {LinkButton("twitter", "sepmotameni")}
+              {LinkButton("whatsapp", "13368995612")}
             </div>
-          )}
-          <div className="mt-4">
-            {LinkButton("instagram", "sepmotameni")}
-            {LinkButton("snapchat", "sepmotameni")}
-            {LinkButton("tiktok", "sepmotameni")}
-            {LinkButton("cashapp", "sdmotameni")}
-            {LinkButton("linkedin", "sepmotameni")}
-            {LinkButton("venmo", "sdmotameni")}
-            {LinkButton("twitter", "sepmotameni")}
-            {LinkButton("whatsapp", "13368995612")}
           </div>
-        </div>
-        <Footer />
-      </main>
+          <Footer />
+        </main>
+      </div>
     );
   }
 }

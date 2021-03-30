c776325776cb27ffb68a944d063e2bbaad23061d
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-30 12:39:15 -0400

    Added Telly Logo to loading screen

--- components/loading.js
+++ components/loading.js
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 export default function Loading() {
   return (
     <div className="flex items-center justify-center w-full h-screen bg-gray-100">
       <svg
-        className="w-10 h-10 text-blue-500 animate-spin"
+        className="absolute w-20 h-20 text-blue-500 animate-spin"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -21,6 +23,7 @@ export default function Loading() {
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         ></path>
       </svg>
+      <Image src="/logo.png" width={57} height={57} alt="" />
     </div>
   );
 }

7fd08ffe638f7760d9c47b378fda440967d5dcb8
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-30 22:33:39 -0400

    more /me details

--- pages/me.js
+++ pages/me.js
@@ -1,16 +1,21 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Profile from "../components/profile";
 import Loading from "../components/loading";
 import UserService from "../services/userService";
 
-import { useEffect, useState } from "react";
-
 export default function Me() {
   const [component, setComponent] = useState(<Loading />);
+  const router = useRouter();
 
   useEffect(() => {
-    UserService.getMe().then(({ data }) => {
-      setComponent(<Profile data={data} isAdmin={true} />);
-    });
+    UserService.getMe()
+      .then(({ data }) => {
+        setComponent(<Profile data={data} isAdmin={true} />);
+      })
+      .catch(() => {
+        router.push("/login");
+      });
   }, []);
 
   return component;

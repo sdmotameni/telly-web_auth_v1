05816be4b2e5de615a45fa7dbbadc2defbf942d1
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-30 22:27:32 -0400

    Added loading to /me

--- pages/me.js
+++ pages/me.js
@@ -1,16 +1,17 @@
 import Profile from "../components/profile";
+import Loading from "../components/loading";
 import UserService from "../services/userService";
 
 import { useEffect, useState } from "react";
 
 export default function Me() {
-  const [data, setData] = useState("");
+  const [component, setComponent] = useState(<Loading />);
+
   useEffect(() => {
     UserService.getMe().then(({ data }) => {
-      setData(data);
-      console.log(data);
+      setComponent(<Profile data={data} isAdmin={true} />);
     });
   }, []);
 
-  return <Profile data={data} isAdmin={true} />;
+  return component;
 }

fcff09f7ab78376beafb83e8326ba64a68656fab
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-30 20:01:54 -0400

    Index

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
-import Head from "next/head";
+import React, { useEffect } from "react";
+import Router from "next/router";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <div className="border border-red-500">hello</div>
-      </main>
-
-      <footer></footer>
-    </div>
-  );
+  useEffect(() => {
+    const { pathname } = Router;
+    if (pathname == "/") {
+      Router.push("/login");
+    }
+  });
+  return <div />;
 }
